6eb119247953f3d0628cded1bce1bc2ef67f6858
Hai Dang <sorrydang2k3@gmail.com>
2025-06-18 02:15:54 +0700

    Update the twelfth day - Create navigation bar

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import "./Login.scss";
+
+const Login = (props) => {
+  return (
+    <div className="login-container">
+      Login
+      <button>Test Bootstrap</button>
+    </div>
+  );
+};
+
+export default Login;
