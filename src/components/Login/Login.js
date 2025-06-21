20dec3201743aedb894093e3e3cc00378798a479
Hai Dang <sorrydang2k3@gmail.com>
2025-06-22 01:33:39 +0700

    Update the thirteenth - Create layout for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,36 @@ import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container">
-      Login
-      <button>Test Bootstrap</button>
+    <div className="login-container mt-3">
+      <div class="container">
+        <div className="row">
+          <div className="content-left col-7">
+            <div className="brand">lighthouse23_</div>
+            <div className="detail">
+              lighthouse23_ helps you connect and share with the people in
+              yourlife.
+            </div>
+          </div>
+          <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">Forgot your password</span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
