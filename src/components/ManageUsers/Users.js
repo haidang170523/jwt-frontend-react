49010693608c8b00790ce408929b306902870f58
Hai Dang <sorrydang2k3@gmail.com>
2025-07-31 01:34:10 +0700

    Update the thirty-first day - Use cookie by cookie-parser - 11.5

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -27,7 +27,6 @@ const Users = (props) => {
 
   const fetchUsers = async () => {
     let response = await fetchAllUsers(currentPage, currentLimit);
-    console.log(">>> Check response: ", response);
     if (response && +response.EC === 0) {
       setTotalPages(response.DT.totalPages);
       setListUsers(response.DT.users);
